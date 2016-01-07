5f4e0b12fc203aa2235543ff2b66d69a9de33a4d
Stan <bernardstanislas@gmail.com>
2016-01-07 11:36:04 +0100

    Fix #16

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import {Provider} from 'react-redux';
 import Comments from './component';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
+// Check if we are bundling. If yes, package.json is found in ../ If no, then we are babelifying so it is in ./
+const packageJsonPath = process.env.BUNDLING ? '..' : '.'
+const infos = require(`${packageJsonPath}/package.json`);
+
 const store = createStore();
 
-module.exports = props => !__DEV__ ? (
+const FocusComments = props => !__DEV__ ? (
     <Provider store={store}>
         <Comments {...props}/>
     </Provider>
@@ -20,3 +24,7 @@ module.exports = props => !__DEV__ ? (
         </DebugPanel>
     </div>
 );
+
+FocusComments.VERSION = infos.version;
+
+module.exports = FocusComments;
