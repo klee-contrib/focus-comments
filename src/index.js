17a48b417d156c7495c31b9bb14958724400cd75
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-21 11:22:44 +0200

    fix issue with package.json in index

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import createStore from './store';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import Comments from './component';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
-const infos = require(`${__PACKAGE_JSON_PATH__}package.json`);
+// const infos = require(`${__PACKAGE_JSON_PATH__}package.json`);
 
 const store = createStore();
 
 let reduxDebug;
 try {
     reduxDebug = __REDUX_DEBUG__;
-} catch(error) {
+} catch (error) {
     reduxDebug = false;
 }
 
 const FocusComments = props => !reduxDebug ? (
     <Provider store={store}>
-        <Comments {...props}/>
+        <Comments {...props} />
     </Provider>
 ) : (
     <div>
-        <Provider store={store}>
-            <Comments {...props}/>
-        </Provider>
-        <DebugPanel top right bottom>
-            <DevTools store={store} monitor={LogMonitor} />
-        </DebugPanel>
-    </div>
-);
+            <Provider store={store}>
+                <Comments {...props} />
+            </Provider>
+            <DebugPanel top right bottom>
+                <DevTools store={store} monitor={LogMonitor} />
+            </DebugPanel>
+        </div>
+    );
 
-FocusComments.VERSION = infos.version;
+FocusComments.VERSION = '';// infos.version;
 
 module.exports = FocusComments;
